6eab90e7e4c888ab03009fcc2261a07b27a820b6
Dev Punjabi <devpanjabi18@gnu.ac.in>
2021-01-28 08:35:04 +0000

    vo

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Current weather by zip code</p>
+        <p className="p-3 text-light h3 bg-primary">We can see the weather here</p>
     </div>
 }
   
